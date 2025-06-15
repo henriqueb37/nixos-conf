bb90a2eaab598add5f950083db882c56065b1e34
José Henrique Brito Oliveira <hriquebrito37@outlook.com>
2025-06-15 17:11:37 -0300

    Made efi on grub optional

--- nixos/modules/grub.nix
+++ nixos/modules/grub.nix
@@ -1,14 +1,26 @@
-{ ... }: {
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
+{
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    grub.efi.enable = lib.mkEnableOption {
+      description = "Whether to enable efi config for grub.";
     };
-    grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
-      useOSProber = true;
+  };
+
+  config = {
+    boot.loader = {
+      efi = lib.mkIf config.grub.efi.enable {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+      grub = {
+        enable = true;
+        efiSupport = lib.mkIf config.grub.efi.enable true;
+        device = "nodev";
+        useOSProber = true;
+      };
     };
   };
 }
