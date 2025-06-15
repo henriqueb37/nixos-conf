287ca3cafb9a5f60c30389c7d7c483eb655aa6bc
José Henrique Brito Oliveira <hriquebrito37@outlook.com>
2025-06-15 15:52:34 -0300

    Added options to gaming module

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -1,18 +1,39 @@
 {
   pkgs,
+  lib,
+  config,
   ...
 }: {
-  programs.gamemode.enable = true;
-  programs.gamescope.enable = true;
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
+  options = {
+    gaming.gamemode.enable = lib.mkEnableOption {
+      gaming.description.enable = "Whether to enable gamemode.";
+      gaming.default.enable = true;
+    };
+    gaming.gamescope.enable = lib.mkEnableOption {
+      gaming.description.enable = "Whether to enable gamescope.";
+      gaming.default.enable = true;
+    };
+    gaming.steam.enable = lib.mkEnableOption {
+      gaming.description.enable = "Whether to enable steam.";
+      gaming.default.enable = true;
+    };
+    gaming.heroic.enable = lib.mkEnableOption {
+      gaming.description.enable = "Whether to enable heroic games launcher.";
+      gaming.default.enable = true;
+    };
+  };
+
+  config.programs.gamemode.enable = lib.mkIf config.gaming.gamemode.enable true;
+  config.programs.gamescope.enable = lib.mkIf config.gaming.gamescope.enable true;
+  config.programs.steam.enable = lib.mkIf config.gaming.steam.enable true;
+  config.programs.steam.gamescopeSession.enable = lib.mkIf config.gaming.steam.enable && config.gaming.gamescope.enable true;
   environment = {
-    sessionVariables = {
+    sessionVariables = lib.mkIf config.gaming.steam.enable {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS =
         "/home/henrique/.steam/root/compatibilitytools.d/";
     };
-    systemPackages = with pkgs; [
-      heroic
-    ];
+    systemPackages = lib.mkIf config.gaming.heroic.enable (with pkgs; [
+        heroic
+    ]);
   };
 }
