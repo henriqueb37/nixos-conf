8f52cbf290ab5c19285f866b2afed298cb2d0a92
José Henrique Brito Oliveira <hriquebrito37@outlook.com>
2025-06-21 22:22:13 -0300

    Fixed gaming nixos module and added option to install minecraft

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -21,19 +21,23 @@
       description = "Whether to enable heroic games launcher.";
       default = true;
     };
+    myNixOS.gaming.minecraft.enable = lib.mkOption {
+      description = "Whether to enable heroic games launcher.";
+      default = false;
+    };
   };
 
   config.programs.gamemode.enable = lib.mkIf config.myNixOS.gaming.gamemode.enable true;
   config.programs.gamescope.enable = lib.mkIf config.myNixOS.gaming.gamescope.enable true;
   config.programs.steam.enable = lib.mkIf config.myNixOS.gaming.steam.enable true;
   config.programs.steam.gamescopeSession.enable = lib.mkIf (config.myNixOS.gaming.steam.enable && config.myNixOS.gaming.gamescope.enable) true;
-  environment = {
+  config.environment = {
     sessionVariables = lib.mkIf config.myNixOS.gaming.steam.enable {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS =
         "/home/henrique/.steam/root/compatibilitytools.d/";
     };
-    systemPackages = lib.mkIf config.myNixOS.gaming.heroic.enable (with pkgs; [
-        heroic
-    ]);
+    systemPackages = with pkgs; [] ++
+      lib.optional config.myNixOS.gaming.heroic.enable heroic ++
+      lib.optional config.myNixOS.gaming.minecraft.enable prismlauncher;
   };
 }
