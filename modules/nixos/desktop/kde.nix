369a3c7d393c4ef20b524e1eb656c80b1a6271dd
José Henrique Brito Oliveira <hriquebrito37@outlook.com>
2026-03-10 23:44:33 -0300

    fix: Kde packages are now declared correctly

--- modules/nixos/desktop/kde.nix
+++ modules/nixos/desktop/kde.nix
@@ -7,8 +7,8 @@
       services.displayManager.sddm.wayland.enable = true;
       services.desktopManager.plasma6.enable = true;
       environment.systemPackages = with pkgs.kdePackages; [
-        kdePackages.kclock
-        kdePackages.kolourpaint
+        kclock
+        kolourpaint
       ];
     };
 }
