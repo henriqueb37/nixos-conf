a0834c0fe16807c8d263775d8d34d74859f94359
José Henrique Brito Oliveira <hriquebrito37@outlook.com>
2026-03-14 03:00:15 -0300

    fix(h/emacs): now using the correct svelte treesitter

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -2,6 +2,18 @@
   flake.modules.homeManager.emacs =
     { config, pkgs, ... }:
     let
+      # This is for svelte-ts-mode
+      tree-sitter-grammars-svelte = pkgs.tree-sitter.buildGrammar {
+        language = "svelte";
+        version = "v1.0.2";
+        src = pkgs.fetchFromGitHub {
+          owner = "tree-sitter-grammars";
+          repo = "tree-sitter-svelte";
+          rev = "774a65aea563accc35f5d45fafa4d96ec5761f57";
+          hash = "sha256-mkw3s0pZQ6ry+fiTk2fJeKVA7Nqyv2Z2R1AFZknzpFM=";
+        };
+      };
+
       emacs = (
         (pkgs.emacsPackagesFor (
           pkgs.emacs.override {
@@ -12,23 +24,25 @@
         )).emacsWithPackages
           (p: [
             p.vterm
-            (p.treesit-grammars.with-grammars (
-              grammars: with grammars; [
-                tree-sitter-nix
-                tree-sitter-c
-                tree-sitter-cpp
-                tree-sitter-python
-                tree-sitter-java
-                tree-sitter-javascript
-                tree-sitter-typescript
-                tree-sitter-jsdoc
-                tree-sitter-svelte
-                tree-sitter-css
-                tree-sitter-yaml
-                tree-sitter-rust
-                tree-sitter-tsx
-              ]
-            ))
+            (
+              p.treesit-grammars.with-grammars (
+                grammars: with grammars; [
+                  tree-sitter-nix
+                  tree-sitter-c
+                  tree-sitter-cpp
+                  tree-sitter-python
+                  tree-sitter-java
+                  tree-sitter-javascript
+                  tree-sitter-typescript
+                  tree-sitter-jsdoc
+                  tree-sitter-css
+                  tree-sitter-yaml
+                  tree-sitter-rust
+                  tree-sitter-tsx
+                  tree-sitter-grammars-svelte
+                ]
+              )
+            )
           ])
       );
       emacs-deps = with pkgs; [
